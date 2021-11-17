76a7b4d2b0eaae08ff2ad198c8f8f321715fe557
herokudev <ao43085@gmail.com>
2021-11-17 17:01:08 -0600

    Add code review changes
    Add code review changes

--- student.rb
+++ student.rb
@@ -3,8 +3,10 @@ require './person'
 class Student < Person
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
-    @classroom = classroom
+    @classroom = classroom.add_student(self) unless classroom.nil?
   end
+
+  attr_accessor :classroom
 
   def play_hooky
     '¯\(ツ)/¯'
